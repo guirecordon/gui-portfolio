ff00bf9c6faa2715ab74473fa6e6be41c9c6c5eb
Gui Recordon <guilherme.recordon@gmail.com>
2023-02-14 15:43:39 -0300

    Add cefet experience to about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -22,12 +22,12 @@ const data = [
     title: 'Full-time Coding Program',
     description: '1300+ hours of Fullstack pair programming',
     body: [
-      'The first stop I made after deciding to move forward with a career change into web development was at Microverse. It is this full-time online program for aspiring developers from the four corners of the world. I learned Javascript to pass the coding challenges and get accepted into the progam.',
-      'Once I was in, I was grouped with other three developers. In my case it was me from Rio de Janeiro, Brazil; a former graphical designer from Mexico city; a newly graduated engineer from Lima, Peru; and a former embeded systems engineer from Lagos, Nigeria.',
-      "It was quite exciting. We met every morning over Zoom for a standup meeting and every night for a wrap-up meeting. A couple months in, covid restrictions were imposed and the timing couldn't feel more appropriate to be inside all day every day and 100% focused on acquiring these new abilities with newly made international friends.",
-      'It was a blast! There was a sense of commitment and of conclusion as each module came to an end.',
-      'Pair-programming was the norm. We did React/Redux on the frond-end and Ruby on Rails in the back-end--in spite of popular outcries to go another direction.',
-      'I got to become fluent in git and github, got accostumed to having code reviewed, and a number of other in-demand skills that I believe have prepared me for my first job in development',
+      'The first stop I made after deciding to move forward with a career change into web development was at Microverse. It is a full-time online program for aspiring developers from all over the world. I learned Javascript to pass the coding challenges and get accepted into the program.',
+      'Once I was in, I joined a team with three other developers. In my case, it was me from Rio de Janeiro, Brazil; a former graphical designer from Mexico City; a newly graduated engineer from Lima, Peru; and a former embedded systems engineer from Lagos, Nigeria.',
+      "It was rather exciting. We met every morning over Zoom for a standup meeting and every night for a wrap-up meeting. A couple of months in, covid hit, and the timing couldn't feel more appropriate to be inside all day, every day, and 100% focused on acquiring these new abilities with newly made international friends.",
+      'It was a blast! There was a sense of commitment and of conclusion as each module ended.',
+      'Pair programming was the norm. We did React/Redux on the front end and Ruby on Rails in the back end--despite unceasing requests to go another direction.',
+      'I became fluent in git and GitHub, got accustomed to having code reviewed, and developed other in-demand skills that I believe have prepared me for my first job in development.',
     ],
     img: microverseLogo,
     video: '',
@@ -38,11 +38,10 @@ const data = [
     title: 'BYU MBA',
     description: 'Marriott School of Business',
     body: [
-      "I can't adequately describre how good it was to go to BYU. A career move was in the makings, but I wasn't quite sure what it was going to be yet. Let alone that it would continue to this day.",
-      'My experience at Brigham Young University was nothing short of awesome. It is one of my favorite places on earth. Sure, there were the beautiful mountains, the feeling of breathing thin air, the ever present blue skies, the desert summer nights. But it was also so much more.',
-      'The important thing for us here, though, is that it was at BYU that I took my first ever class in programming. It was an Intro to Java elective.',
-      'The seed had been planted and it was going to swell in due time.',
-      'Some of the transferrable skills I acquired over that period of time were: a stronger business acumen, better presentation skills, team work was over-emphasized and brought to a whole new level, a stronger sense for personal accountability, the experience of working in the United States, exposure to different cultures, the list goes on and on.',
+      "I can't adequately describe how good it was to go to BYU. A career move was in the making, although I wasn't sure what it would be. Let alone that it would continue to this day.",
+      'My experience at Brigham Young University was nothing short of incredible. It is one of my favorite places on earth. Sure, there were the beautiful mountains, the feeling of breathing thin air, the ever-present blue skies, and the desert summer nights. But it was also so much more.',
+      'The important thing for us here, though, is that it was at BYU that I took my first-ever class in programming. It was an Intro to Java elective. That class planted the seed that was going to sprout in due time.',
+      'Some of the transferrable skills I acquired over that time were strong business acumen; better presentation skills; teamwork--as it was over-emphasized and brought to a whole new level; a strong sense of personal accountability; the experience of working in the United States; exposure to different cultures; the list goes on and on.',
     ],
     img: byuLogo,
     video: '',
@@ -53,9 +52,10 @@ const data = [
     title: 'Catholic University of Petropolis',
     description: 'Business School - Undergraduate Degree',
     body: [
-      "My undergraduate degree was in business. At the time, my eyes hadn't been opened to the opportunities in technology yet. It wasn't until I moved to Utah years later where I got to witness the emergence of the so called Silicon slopes in Utah Valley that I would begin to realize the greatness of opportunities in this industry.",
-      'In spite of the career change that would later take place and bring me to where I am today, I feel satisfied with the educational and professional decisions I made then. I took real advantage of it. I got to participate in important international projects. I worked for a French multinational that would send me to France for yearly conferences, I got to learn the language. It was nothing short of a lot of fun and learning.',
-      'Some of the most important transferrable skills I picked up during that stage of my life were in terms of relationship. Establishing relationships with different stake holders was important, setting up expectations, staying true to commitments and goals. As I look back, I see how foundational it was for me to have those experiences. I feel like I can transfer all of these skills over to my current position as a software developer and be much more than just a coder, but an active team player that understands the nuances of daily corporate challenges.',
+      "My undergraduate degree was in business. At the time, my eyes weren't open to opportunities in technology yet. It wasn't until I moved to Utah years later that I witnessed the emergence of the so-called Silicon slopes in Utah Valley. Then I would begin to realize the greatness of opportunities in this industry.",
+      'Despite the career change that would later take place and bring me to where I am today, I feel satisfied with the educational and professional decisions I made then. I took real advantage of it. I got to participate in important international projects. I worked for a French multinational that would send me to France for yearly conferences, and I got to learn the language. It was nothing short of a lot of fun and learning.',
+      'One key competence I learned during that stage of my life was people skills. Establishing relationships with different stakeholders was paramount. Things such as building expectations, and staying true to commitments and goals.',
+      'As I look back, I see how foundational it was for me to have those experiences. I feel like I can transfer these skills to my current position as a software developer and become something more, such as an active team player that understands the nuances of daily corporate challenges.',
     ],
     img: ucpLogo,
     video: '',
@@ -66,10 +66,10 @@ const data = [
     title: 'Federal Institute of Technology - Rio de Janeiro',
     description: 'Computer Engineering Undergraduate Program',
     body: [
-      "More recently, I took the Brazilian national admittance exam of higher education, which is not easy, especially for someone who'd been out of school for so long and got accepted into the Federal Institute of Technology to study computer engineering.",
-      'However, a few months in, after studying carefully the syllabus of all semesters, I realized there was a gap between what the market was asking for and what academia was teaching. Of course, they had some super fun classes that I could look forward to. But, with my focus on Javascript, really everything that I wanted and needed to use in the market place I would have to learn on my own.',
-      'So, already having an undergraduate and a graduate degree under my belt, I decided to drop out of the Computer Engineering school and continue with my self learning approach to computer science.',
-      'I know this topic splits opinions a little bit, but I think it was the right decision for me. It was eye opening to witness the state of affairs of computer science education. I still consider the possibility of going back for a masters in computer science, but, when I do, with a more specific technology focus.',
+      "More recently, I took the Brazilian national admittance exam for higher education, which is not easy, especially for someone who'd been out of school for so long and got accepted into the Federal Institute of Technology to study computer engineering.",
+      'However, a few months in, after carefully studying the syllabus of all semesters, I realized there was a gap between what the market was asking for and what academia was teaching. Of course, they had some super fun classes that I could look forward to, but with my focus on Javascript, really, everything that I wanted and needed to use in the marketplace, I would have to learn on my own.',
+      'So, already having an undergraduate and a graduate degree under my belt, I decided to drop out of the Computer Engineering school and continue with my self-learning approach to computer science.',
+      "I know this topic splits opinions a little bit, but I think it was the right decision for me. Plus, it was eye-opening to witness the state of affairs of computer science education. I still consider the possibility of returning for a master's in computer science, but when I do, I want to go in for a more specific technology focus.",
     ],
     img: cefetLogo,
     video: '',
@@ -123,11 +123,11 @@ export function About() {
 
             <p>
               Please select an option at the left to read a little about some of
-              the educational experiences I've had. I'll be honest, as I was
-              putting this together, it was challenging to draw a straight line
-              between my past experiences. So, I didn't try to. I decided to
-              embrace my career journey in all its complexity. I've always been
-              a learner, and that's my biggest ambition.
+              the experiences I've had. I'll be honest, as I was putting this
+              together, it was challenging to draw a straight line between my
+              past experiences. So, I didn't try to. I decided to embrace my
+              career journey in all its complexity. I've always been a learner,
+              and that's my biggest ambition.
             </p>
             <p>
               It's been an exciting time for me to learn development, and
