1f4d84aae94f194883e14c12587d5fc603b9c784
Gui Recordon <guilherme.recordon@gmail.com>
2023-02-14 21:40:36 -0300

    Add new colors

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -14,6 +14,20 @@ export const AboutContainer = styled.div`
     text-shadow: 2px 2px #67e8f9;
 
     background-image: linear-gradient(
+      250deg,
+      hsl(199deg 95% 74%) 0%,
+      hsl(199deg 76% 69%) 3%,
+      hsl(199deg 63% 63%) 9%,
+      hsl(198deg 53% 58%) 16%,
+      hsl(198deg 46% 53%) 25%,
+      hsl(197deg 44% 48%) 38%,
+      hsl(197deg 48% 42%) 53%,
+      hsl(196deg 52% 37%) 70%,
+      hsl(195deg 59% 32%) 86%,
+      hsl(194deg 70% 27%) 100%
+    );
+    /* 
+    background-image: linear-gradient(
       45deg,
       hsl(350deg 89% 60%) 0%,
       hsl(350deg 91% 60%) 11%,
@@ -25,7 +39,7 @@ export const AboutContainer = styled.div`
       hsl(349deg 99% 54%) 78%,
       hsl(349deg 99% 53%) 89%,
       hsl(347deg 100% 50%) 100%
-    );
+    ); */
 
     background-size: 100%;
     background-repeat: repeat;
@@ -45,11 +59,14 @@ export const MainContainer = styled.div`
 
   height: 90%;
 
-  border: solid 2px #5b21b6;
+  /* border: solid 2px #5b21b6; */
+  border: solid 3px #155e75;
+
   border-radius: 8px;
   overflow: hidden;
 
-  box-shadow: 2px 2px 4px #fde047;
+  /* box-shadow: 2px 2px 4px #fde047; */
+  box-shadow: 2px 2px 4px #67e8f9;
 `;
 
 export const AboutCardsContainer = styled.div`
@@ -101,6 +118,20 @@ export const AboutDescriptionContainer = styled.div`
     padding-bottom: 1rem;
 
     background-image: linear-gradient(
+      250deg,
+      hsl(199deg 95% 74%) 0%,
+      hsl(199deg 76% 69%) 3%,
+      hsl(199deg 63% 63%) 9%,
+      hsl(198deg 53% 58%) 16%,
+      hsl(198deg 46% 53%) 25%,
+      hsl(197deg 44% 48%) 38%,
+      hsl(197deg 48% 42%) 53%,
+      hsl(196deg 52% 37%) 70%,
+      hsl(195deg 59% 32%) 86%,
+      hsl(194deg 70% 27%) 100%
+    );
+
+    /* background-image: linear-gradient(
       45deg,
       hsl(350deg 89% 60%) 0%,
       hsl(350deg 91% 60%) 11%,
@@ -112,7 +143,7 @@ export const AboutDescriptionContainer = styled.div`
       hsl(349deg 99% 54%) 78%,
       hsl(349deg 99% 53%) 89%,
       hsl(347deg 100% 50%) 100%
-    );
+    ); */
 
     background-size: 100%;
     background-repeat: repeat;
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -14,6 +14,20 @@ export const ContactContainer = styled.div`
     text-shadow: 2px 2px #67e8f9;
 
     background-image: linear-gradient(
+      250deg,
+      hsl(199deg 95% 74%) 0%,
+      hsl(199deg 76% 69%) 3%,
+      hsl(199deg 63% 63%) 9%,
+      hsl(198deg 53% 58%) 16%,
+      hsl(198deg 46% 53%) 25%,
+      hsl(197deg 44% 48%) 38%,
+      hsl(197deg 48% 42%) 53%,
+      hsl(196deg 52% 37%) 70%,
+      hsl(195deg 59% 32%) 86%,
+      hsl(194deg 70% 27%) 100%
+    );
+
+    /* background-image: linear-gradient(
       45deg,
       hsl(350deg 89% 60%) 0%,
       hsl(350deg 91% 60%) 11%,
@@ -25,7 +39,7 @@ export const ContactContainer = styled.div`
       hsl(349deg 99% 54%) 78%,
       hsl(349deg 99% 53%) 89%,
       hsl(347deg 100% 50%) 100%
-    );
+    ); */
 
     background-size: 100%;
     background-repeat: repeat;
@@ -43,6 +57,19 @@ export const ContactContainer = styled.div`
 
   label {
     background-image: linear-gradient(
+      250deg,
+      hsl(199deg 95% 74%) 0%,
+      hsl(199deg 76% 69%) 3%,
+      hsl(199deg 63% 63%) 9%,
+      hsl(198deg 53% 58%) 16%,
+      hsl(198deg 46% 53%) 25%,
+      hsl(197deg 44% 48%) 38%,
+      hsl(197deg 48% 42%) 53%,
+      hsl(196deg 52% 37%) 70%,
+      hsl(195deg 59% 32%) 86%,
+      hsl(194deg 70% 27%) 100%
+    );
+    /* background-image: linear-gradient(
       45deg,
       hsl(85deg 85% 35%) 0%,
       hsl(80deg 82% 37%) 11%,
@@ -54,7 +81,7 @@ export const ContactContainer = styled.div`
       hsl(57deg 70% 54%) 78%,
       hsl(54deg 82% 59%) 89%,
       hsl(50deg 98% 64%) 100%
-    );
+    ); */
 
     background-size: 100%;
     background-repeat: repeat;
@@ -69,13 +96,16 @@ export const ContactContainer = styled.div`
     width: 100%;
     display: block;
     margin-bottom: 1rem;
-    border: 2px solid #84cc16;
+    /* border: 2px solid #84cc16; */
+    border: solid 3px #155e75;
+
     border-radius: 8px;
     font-size: 1rem;
     padding: 0.5rem;
 
     &:focus {
-      border: 2px solid #65a30d;
+      /* border: 2px solid #65a30d; */
+      border: solid 3px #155e75;
     }
   }
 
@@ -86,7 +116,9 @@ export const ContactContainer = styled.div`
     font-size: 1rem;
     padding: 0.5rem;
     margin-bottom: 1rem;
-    border: 2px solid #84cc16;
+    /* border: 2px solid #84cc16; */
+    border: solid 3px #155e75;
+
     border-radius: 8px;
   }
 
@@ -96,11 +128,26 @@ export const ContactContainer = styled.div`
     font-size: 1rem;
     font-family: 'Roboto', sans-serif;
     cursor: pointer;
-    border: solid 2px #84cc16;
+    /* border: solid 2px #84cc16; */
+    border: solid 3px #155e75;
+
     border-radius: 8px;
     color: #ecfccb;
     font-weight: 700;
     background-image: linear-gradient(
+      250deg,
+      hsl(199deg 95% 74%) 0%,
+      hsl(199deg 76% 69%) 3%,
+      hsl(199deg 63% 63%) 9%,
+      hsl(198deg 53% 58%) 16%,
+      hsl(198deg 46% 53%) 25%,
+      hsl(197deg 44% 48%) 38%,
+      hsl(197deg 48% 42%) 53%,
+      hsl(196deg 52% 37%) 70%,
+      hsl(195deg 59% 32%) 86%,
+      hsl(194deg 70% 27%) 100%
+    );
+    /* background-image: linear-gradient(
       45deg,
       hsl(85deg 85% 35%) 0%,
       hsl(85deg 73% 39%) 11%,
@@ -112,10 +159,26 @@ export const ContactContainer = styled.div`
       hsl(83deg 69% 60%) 78%,
       hsl(82deg 76% 64%) 89%,
       hsl(82deg 85% 67%) 100%
-    );
+    ); */
 
     &:hover {
       background-image: linear-gradient(
+        250deg,
+        hsl(45deg 93% 47%) 0%,
+        hsl(46deg 85% 50%) 3%,
+        hsl(46deg 86% 52%) 9%,
+        hsl(46deg 88% 54%) 16%,
+        hsl(47deg 89% 56%) 25%,
+        hsl(48deg 91% 57%) 38%,
+        hsl(48deg 92% 59%) 53%,
+        hsl(49deg 94% 60%) 70%,
+        hsl(50deg 96% 62%) 86%,
+        hsl(50deg 98% 64%) 100%
+      );
+
+      color: #a16207;
+
+      /* background-image: linear-gradient(
         45deg,
         hsl(85deg 85% 35%) 0%,
         hsl(85deg 82% 33%) 11%,
@@ -127,7 +190,7 @@ export const ContactContainer = styled.div`
         hsl(87deg 66% 24%) 78%,
         hsl(87deg 64% 22%) 89%,
         hsl(88deg 61% 20%) 100%
-      );
+      ); */
     }
   }
 `;
@@ -163,7 +226,8 @@ export const SocialBar = styled.div`
   width: 4rem;
   height: 60%;
   border-radius: 8px;
-  border: 3px solid #84cc16;
+  /* border: 3px solid #84cc16; */
+  border: solid 3px #155e75;
 
   display: flex;
   justify-content: center;
@@ -175,7 +239,7 @@ export const SocialBar = styled.div`
 
   a {
     &:hover {
-      box-shadow: 2px 2px 3px #84cc16;
+      box-shadow: 2px 2px 3px #155e75;
     }
   }
 `;
